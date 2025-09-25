c89e11aa460af1faa41a19be2cae3c2ccbbac3b3
CaptainBidou <tomas.salvado_robalo@ig2i.centralelille.fr>
2025-09-25 10:37:55 -0400

    test PWM + controle thermostat

--- Thread_Cligne_10_Hz_time.c
+++ Thread_Cligne_10_Hz_time.c
@@ -20,7 +20,8 @@
 #define SPI_SCK 25 // Définition du GPIO associe au SCK du SPI (broche 22)
 
 double frequence = 10.0; // Hertz : fréquence de clignotement désirée
-double frequence_SPI = 5000000.0; 
+double frequence_SPI = 4000000.0; // Hertz : fréquence du SPI
+double RANGE = 200; // valeur du RANGE du pwm
 
 /* Fonction clignote qui sera appelée par le thread "cligne"
  * 
@@ -34,12 +35,11 @@ void *clignote(int broche)
 	clock_t debut, fin; // Variables de temps
 	double demiPer = 1/(2.0*frequence); // Calcul de la demi-période
 	int etat = 0; // État du DEL
-	int nbClock=0;
 	
 	debut = clock(); // Temps écoulé depuis le lancement du programme
 	
 	// Boucle infinie pour le clignotement du DEL
-	while(nbClock!=32){
+	while(1){
 		if (etat==0){
 			bcm2835_gpio_write(broche, HIGH);
 			etat = 1;
@@ -47,7 +47,6 @@ void *clignote(int broche)
 		else{
 			bcm2835_gpio_write(broche, LOW);
 			etat = 0;
-			nbClock++;
 		}
 		
 		fin = clock(); // Temps écoulé depuis le lancement du programme
@@ -105,12 +104,13 @@ double lecture_SPI(void){
 	while(nbClock!=32){
 		if (etat==0){
 			bcm2835_gpio_write(SPI_SCK, HIGH);
+			readMiso = bcm2835_gpio_lev(SPI_MISO);
+			readMiso = readMiso & 0b00000000000000000000000000000001;
+			sigMiso = (sigMiso << 1) + readMiso;
 			etat = 1;
 		}
 		else{
 			bcm2835_gpio_write(SPI_SCK, LOW);
-			readMiso = bcm2835_gpio_lev(SPI_MISO);
-			sigMiso = (sigMiso << 1) | readMiso;
 			etat = 0;
 			nbClock++;
 		}
@@ -137,24 +137,75 @@ double lecture_SPI(void){
 	double temp = 0.0;
 	// Verification de la temperature negative
 	if((sigMiso&maskNEG)>>31==1){
-		temp = -1*(~((tempENT>>20)*1.0+(tempDEC>>18)*0.25))+1;
+		temp = (double)(-1*(~(unsigned int)((tempENT>>20)*1.0+(tempDEC>>18)*0.25))+1);
 		
 	}
 	else{
-		temp = (tempENT>>20)*1.0+(tempDEC>>18)*0.25;
+		temp = (double)((tempENT>>20)*1.0)+(double)((tempDEC>>18)*0.25);
 	}
 	
 	
-	printf("Signal recupere : %d\n",sigMiso);
-	printf("Signal masquee :%f\n",temp);
-	
-	// Libérer le GPIO
-    bcm2835_close();
+	//printf("Signal recupere : %d\n",sigMiso);
+	//printf("Signal traite :%f\n",temp);
 
 	return (double)temp;
 }
 
 
+int commande_Thermo(double consigne, double mesure){
+	if(consigne < mesure){
+		return 0; // Chauffage desactive
+	}
+	else{
+		return RANGE; // Chauffage active
+	}
+}
+
+
+
+
+
+void pwmThread(){
+	int range = RANGE;
+	int d = 3;
+	int dutyCycle = 0;
+	bcm2835_gpio_fsel(18, BCM2835_GPIO_FSEL_ALT5);
+	bcm2835_pwm_set_clock(d);
+	bcm2835_pwm_set_mode(0,1,1);
+	bcm2835_pwm_set_range(0, range);
+	bcm2835_pwm_set_data(0,0);
+	
+	while(1){
+		while(dutyCycle < range){
+			bcm2835_pwm_set_data(0,dutyCycle);
+			dutyCycle ++;
+			usleep(10000);
+			printf("dutyCycle = %d\n",dutyCycle);
+		}
+		while(dutyCycle>0){
+			bcm2835_pwm_set_data(0,dutyCycle);
+			dutyCycle = dutyCycle-1;
+			usleep(10000);
+			printf("dutyCycle = %d\n",dutyCycle);
+		}
+		  }
+		
+		//bcm2835_gpio_fsel(18, BCM2835_GPIO_FSEL_OUTP);
+		//bcm2835_gpio_write(18, HIGH);
+	
+}
+
+
+
+void temperatureThread(void){
+	
+	while(1){
+		double temp = lecture_SPI();
+		printf("Temperature :%f C\n",temp);
+		sleep(1);
+		}
+	
+}
 
 /* Fonction main
  * 
@@ -166,23 +217,40 @@ double lecture_SPI(void){
 int main(int argc, char **argv)
 {
 	// Identificateur du thread
-	pthread_t cligne;  
+	pthread_t cligne;
+	pthread_t temperature;  
+	pthread_t pwm;
 	
 	// Initialisation du bcm2835
 	if (!bcm2835_init()){
 		return 1;
 	}
 	
-	lecture_SPI();
+	
 	// Configuration du GPIO pour DEL 1 (rouge)
 	bcm2835_gpio_fsel(20, BCM2835_GPIO_FSEL_OUTP);
 	// Configuration du GPIO pour bouton-poussoir 1
 	bcm2835_gpio_fsel(19, BCM2835_GPIO_FSEL_INPT);
 	
+	int range = 600;
+	int d = 3;
+	bcm2835_gpio_fsel(18, BCM2835_GPIO_FSEL_ALT5);
+	bcm2835_pwm_set_clock(d);
+	bcm2835_pwm_set_mode(0,1,1);
+	bcm2835_pwm_set_range(0, range);
+	bcm2835_pwm_set_data(0,0);
+	bcm2835_pwm_set_data(0,600);
+	printf("test\n");
+	
 	// Création du thread "cligne".
 	// Lien avec la fonction clignote.
 	// Cette dernière n'exige pas de paramètres.
 	pthread_create(&cligne, NULL, &clignote, 20);
+	pthread_create(&temperature, NULL, &temperatureThread, NULL);
+	pthread_create(&pwm, NULL, &pwmThread,NULL);
+	
+	
+	
 	
 	// Boucle tant que le bouton-poussoir est non enfoncé
 	while(bcm2835_gpio_lev(19)){
@@ -191,8 +259,12 @@ int main(int argc, char **argv)
 
 	// Si bouton-poussoir enfoncé, arrêt immédiat du thread 
     pthread_cancel(cligne);
+    pthread_cancel(temperature);
+    pthread_cancel(pwm);
     // Attente de l'arrêt du thread
     pthread_join(cligne, NULL);
+    pthread_join(temperature, NULL);
+    pthread_join(pwm, NULL);
     
     // Éteindre le DEL rouge
     bcm2835_gpio_write(20, LOW);
